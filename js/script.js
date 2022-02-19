e533e822eb3e8db2a62b514d9d75ce873dc16ccb
Christian Conchari <chrisconchari@gmail.com>
2022-02-19 10:51:54 -0400

    Modifing all

--- js/script.js
+++ js/script.js
@@ -1,38 +1,9 @@
 $(document).ready(function(){
-
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on('scroll load',function(){
-
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
     }else{
-      $('.top').hide();
+      $('.navbar').removeClass("sticky");
     }
-
-  });
-
-  // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
-    );
-
-  });
-
-});
+  })
+})
